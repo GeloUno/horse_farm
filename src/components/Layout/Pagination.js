70a194f78af44eb2ac0c3414f52bd828e724fa25
GeloUno <geloke59@gmail.com>
2020-09-04 17:12:04 +0200

    pagination is now end without setPage
    TODO: add function to set next page in galery and buttons pagination

--- src/components/Layout/Pagination.js
+++ src/components/Layout/Pagination.js
@@ -1,27 +1,53 @@
 import React from 'react';
 
-const Pagination = ({ numberPage = 10, lengthPages = 10 }) => {
+/* TODO: add function in galery set next page and add functiom with number in button */
+
+const Pagination = ({ numberPage = 1, lengthPages = 1, setNumbetPages }) => {
   return (
     <div>
-      <div className="paggination">
-        <button
-          className="prevArrowButton"
-          disabled={numberPage - 1 <= 1 ? true : false}
-        >
-          <i class="fas fa-arrow-circle-left"></i>
-        </button>
-        <button disabled={true} className="prevNumberButton">
-          {numberPage - 1}
-        </button>
-        <button className="currentNumberButton">{numberPage}</button>
-        <button className="nextNumberButton">{numberPage + 1}</button>
-        <button
-          className="nextArrowButton"
-          disabled={numberPage >= lengthPages ? true : false}
-        >
-          <i class="fas fa-arrow-circle-right"></i>
-        </button>
-      </div>
+      {lengthPages > 1 && (
+        <div className="paggination">
+          <button
+            className="prevArrowButton"
+            disabled={numberPage - 1 < 1 ? true : false}
+          >
+            <i class="fas fa-arrow-circle-left"></i>
+          </button>
+          <button
+            className="prevNumberButton"
+            hidden={numberPage - 2 < 1 ? true : false}
+          >
+            {numberPage - 2}
+          </button>
+          <button
+            className="prevNumberButton"
+            hidden={numberPage - 1 < 1 ? true : false}
+          >
+            {numberPage - 1}
+          </button>
+          <button className="currentNumberButton" style={{ fontWeight: '600' }}>
+            {numberPage}
+          </button>
+          <button
+            className="nextNumberButton"
+            hidden={numberPage + 1 > lengthPages ? true : false}
+          >
+            {numberPage + 1}
+          </button>
+          <button
+            className="nextNumberButton"
+            hidden={numberPage + 2 > lengthPages ? true : false}
+          >
+            {numberPage + 2}
+          </button>
+          <button
+            className="nextArrowButton"
+            disabled={numberPage >= lengthPages ? true : false}
+          >
+            <i class="fas fa-arrow-circle-right"></i>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
