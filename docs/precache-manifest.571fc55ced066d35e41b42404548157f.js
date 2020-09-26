6f69d8cadf28d78c86a6b9d011a741fd1ce550ed
GeloUno <geloke59@gmail.com>
2020-09-26 14:10:38 +0200

    build to docs

---
+++ docs/precache-manifest.571fc55ced066d35e41b42404548157f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85908ddeba22a745c7ffbb10449b39b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "128033e3021a567fd566",
+    "url": "./static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "54a9d84148e637a3fe0d",
+    "url": "./static/css/main.6d19f4d1.chunk.css"
+  },
+  {
+    "revision": "128033e3021a567fd566",
+    "url": "./static/js/2.bf9fe3e5.chunk.js"
+  },
+  {
+    "revision": "8724c659f825330d722cea472220cd6e",
+    "url": "./static/js/2.bf9fe3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a9d84148e637a3fe0d",
+    "url": "./static/js/main.64fa4c5d.chunk.js"
+  },
+  {
+    "revision": "6babae01ef63a94d6579",
+    "url": "./static/js/runtime-main.0b35a3ff.js"
+  },
+  {
+    "revision": "8f51e8a2d8262e47d0b4b8d00953822e",
+    "url": "./static/media/HorseBackground.8f51e8a2.png"
+  },
+  {
+    "revision": "9476513c0b3a8154d54b66ab43577ac2",
+    "url": "./static/media/LoginHorse.9476513c.png"
+  },
+  {
+    "revision": "3d106ca18b279b263525973fc6d846a0",
+    "url": "./static/media/SigninHorse.3d106ca1.png"
+  },
+  {
+    "revision": "537a41c971a6e3b3174f112e3c8f70e3",
+    "url": "./static/media/Stadnina-1.537a41c9.png"
+  },
+  {
+    "revision": "43e82fc0e13d7410ca0195c613fd9bb6",
+    "url": "./static/media/Stadnina-2.43e82fc0.png"
+  },
+  {
+    "revision": "99cc941d7b96884140756ba5449e9d32",
+    "url": "./static/media/Stadnina-3.99cc941d.png"
+  },
+  {
+    "revision": "b642935c51d6bb16ed168de093408847",
+    "url": "./static/media/Stadnina-4.b642935c.png"
+  },
+  {
+    "revision": "72ae665333d3eba6dffd026dcf362d82",
+    "url": "./static/media/Stadnina-5.72ae6653.png"
+  }
+]);
