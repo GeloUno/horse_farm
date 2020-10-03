f49fef7cce060aaf78354ee6c432f7c63ed13a58
GeloUno <geloke59@gmail.com>
2020-10-03 15:03:58 +0200

    build

---
+++ build/precache-manifest.ec52c4cffb3a03600076732d6242df9c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9deb16a7601098cec2f94c3ac194f5f",
+    "url": "/GeloUno/horse_farm/index.html"
+  },
+  {
+    "revision": "128033e3021a567fd566",
+    "url": "/GeloUno/horse_farm/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "3e37526e6bd870c20824",
+    "url": "/GeloUno/horse_farm/static/css/main.6d19f4d1.chunk.css"
+  },
+  {
+    "revision": "128033e3021a567fd566",
+    "url": "/GeloUno/horse_farm/static/js/2.bf9fe3e5.chunk.js"
+  },
+  {
+    "revision": "8724c659f825330d722cea472220cd6e",
+    "url": "/GeloUno/horse_farm/static/js/2.bf9fe3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e37526e6bd870c20824",
+    "url": "/GeloUno/horse_farm/static/js/main.2f892ff7.chunk.js"
+  },
+  {
+    "revision": "258160338f2356f87c24",
+    "url": "/GeloUno/horse_farm/static/js/runtime-main.a64ff748.js"
+  },
+  {
+    "revision": "8f51e8a2d8262e47d0b4b8d00953822e",
+    "url": "/GeloUno/horse_farm/static/media/HorseBackground.8f51e8a2.png"
+  },
+  {
+    "revision": "9476513c0b3a8154d54b66ab43577ac2",
+    "url": "/GeloUno/horse_farm/static/media/LoginHorse.9476513c.png"
+  },
+  {
+    "revision": "3d106ca18b279b263525973fc6d846a0",
+    "url": "/GeloUno/horse_farm/static/media/SigninHorse.3d106ca1.png"
+  },
+  {
+    "revision": "537a41c971a6e3b3174f112e3c8f70e3",
+    "url": "/GeloUno/horse_farm/static/media/Stadnina-1.537a41c9.png"
+  },
+  {
+    "revision": "43e82fc0e13d7410ca0195c613fd9bb6",
+    "url": "/GeloUno/horse_farm/static/media/Stadnina-2.43e82fc0.png"
+  },
+  {
+    "revision": "99cc941d7b96884140756ba5449e9d32",
+    "url": "/GeloUno/horse_farm/static/media/Stadnina-3.99cc941d.png"
+  },
+  {
+    "revision": "b642935c51d6bb16ed168de093408847",
+    "url": "/GeloUno/horse_farm/static/media/Stadnina-4.b642935c.png"
+  },
+  {
+    "revision": "72ae665333d3eba6dffd026dcf362d82",
+    "url": "/GeloUno/horse_farm/static/media/Stadnina-5.72ae6653.png"
+  }
+]);
