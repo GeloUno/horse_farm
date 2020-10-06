17efdb6871e270214475c69a87d65c553a2d2495
GeloUno <geloke59@gmail.com>
2020-10-06 14:11:51 +0200

    Plan booking at work

--- src/components/Booking/PlanBookings.js
+++ src/components/Booking/PlanBookings.js
@@ -4,23 +4,31 @@ import PropTypes from 'prop-types';
 const PlanBookings = (props) => {
   return (
     <div className="container containerPlanBooking">
+      {' '}
       <div className="dateHeader ">
+        {' '}
         <div className="monthYearHeader">
-          <div className="month">wrzesien </div>
-          <div className="year">2020</div>
-        </div>
+          {' '}
+          <div className="month">wrzesien </div>{' '}
+          <div className="year">2020</div>{' '}
+        </div>{' '}
         <div className="dayHeader">
-          <div className="dayNumber">22</div>
-          <div className="daysOfWeek">sobota</div>
-        </div>
-      </div>
+          {' '}
+          <div className="dayNumber">22</div>{' '}
+          <div className="daysOfWeek">sobota</div>{' '}
+        </div>{' '}
+      </div>{' '}
       <div className="clendaryHouerBookingBody">
-        <div className="clendaryBooking"> kalendarz</div>
-        <div className="houerBooking">godziny</div>
-      </div>
+        {' '}
+        <div className="clendaryBooking"> kalendarz</div>{' '}
+        <div className="houerBooking">godziny</div>{' '}
+      </div>{' '}
       <div>
-        <button className="btn btn-green btn-capitalize">rezerwacja</button>
-      </div>
+        {' '}
+        <button className="btn btn-green btn-capitalize">
+          rezerwacja
+        </button>{' '}
+      </div>{' '}
     </div>
   );
 };
