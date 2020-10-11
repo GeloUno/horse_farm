8f262a8afe230bdf05f5bc5a911d6976888f81d0
GeloUno <geloke59@gmail.com>
2020-10-12 01:01:30 +0200

    Preper Dev DataBooking

--- src/DevUtility/booking.js
+++ src/DevUtility/booking.js
@@ -1,29 +1,176 @@
+import faker from 'faker';
+faker.setLocale('pl');
+const date = new Date();
+
 export const DevBookingData = [
-  { id: 11111, name: 'tomas' },
-  { id: 11112, name: 'admas' },
-  { id: 11113, name: 'jsons' },
-  { id: 11114, name: 'romans' },
-];
-export const DevExampleBookingData = [
   {
-    _id: 'y11111',
-    year: 2020,
-    month: 2,
-    houer: 16,
-    users: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 6 }, { id: 7 }],
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
   },
   {
-    _id: 'z11111',
-    year: 2020,
-    month: 2,
-    houer: 17,
-    users: [
-      { id: 8 },
-      { id: 11 },
-      { id: 13 },
-      { id: 18 },
-      { id: 12 },
-      { id: 1 },
-    ],
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    hourBooking: new Date(
+      faker.random.number({ min: 2019, max: 2023 }),
+      faker.random.number({ min: 0, max: 11 }),
+      faker.random.number({ min: 1, max: 31 }),
+      faker.random.number({ min: 8, max: 21 })
+    ),
   },
 ];
