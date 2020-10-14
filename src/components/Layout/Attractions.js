3246dc2e28ba3f2ba824ae1f667fae7f41256a17
GeloUno <geloke59@gmail.com>
2020-10-14 09:48:16 +0200

    Move data Atraction to utility

--- src/components/Layout/Attractions.js
+++ src/components/Layout/Attractions.js
@@ -1,57 +1,5 @@
 import React, { useState } from 'react';
-import atrattionImage_1 from '../../assets/Attractions/Stadnina-1.png';
-import atrattionImage_2 from '../../assets/Attractions/Stadnina-2.png';
-import atrattionImage_3 from '../../assets/Attractions/Stadnina-3.png';
-import atrattionImage_4 from '../../assets/Attractions/Stadnina-4.png';
-import atrattionImage_5 from '../../assets/Attractions/Stadnina-5.png';
-
-const dataAtractions = {
-  1: {
-    titleRegular: 'Jazda',
-    titleBold: 'Konna',
-    description:
-      'serdecznie zapraszamy na naukę jazdy konnej, zarówno tych dużych jak i najmniejszych, tych z doświadczonych, oraz tych którzy chcą się nauczyć tego pięknego sportu',
-    backgroundText: 'konie',
-    image: atrattionImage_1,
-    page: 1,
-  },
-  2: {
-    titleRegular: 'Dla',
-    titleBold: 'Juniora',
-    description:
-      'z nami naukę jazdy konnej możesz już zacząć od najmłodszych lat',
-    backgroundText: 'to',
-    image: atrattionImage_2,
-    page: 2,
-  },
-  3: {
-    titleRegular: 'Tutaj',
-    titleBold: 'Trenujemy',
-    description:
-      'na tym wybiegu najmłodsi uczą się zdobywać pierwsze szlify jazdy konnej jak również ci doświadczeni mogą trochę poszaleć',
-    backgroundText: 'Nasza',
-    image: atrattionImage_3,
-    page: 3,
-  },
-  4: {
-    titleRegular: 'Razem w',
-    titleBold: 'Teren',
-    description:
-      'jeśli pod okiem instruktora zdobędziesz odpowiednie doświadczenie ruszymy na jazdę konną w teren po malowniczym krajobrazie Lubelszczyzny',
-    backgroundText: 'pasja',
-    image: atrattionImage_4,
-    page: 4,
-  },
-  5: {
-    titleRegular: 'Po',
-    titleBold: 'Treningu',
-    description:
-      'a wieczorkiem usiądźmy razem przy ognisku by zregenerować siły na kolejną jazdę konną',
-    backgroundText: 'życia',
-    image: atrattionImage_5,
-    page: 5,
-  },
-};
+import { dataAtractions } from '../../utility/dataAtractons';
 const nextPage = (numberPage) => {
   if (numberPage >= 5) {
     return dataAtractions[1];
@@ -61,7 +9,6 @@ const nextPage = (numberPage) => {
 };
 
 const equalsPageNumberReturnOpacity = (pageNumber, buttonNumber) => {
-  // eslint-disable-next-line eqeqeq
   return buttonNumber == pageNumber ? { opacity: '1' } : { opacity: '.15' };
 };
 
