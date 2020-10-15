d870926e39c565db71267c19ae0412e55d63e7ff
GeloUno <geloke59@gmail.com>
2020-10-15 22:32:16 +0200

    Move date dev to DEVUtility

---
+++ src/DevUtility/opinion.js
@@ -0,0 +1,58 @@
+export const DEVOpinionData = [
+  {
+    name: 'Roger',
+    description:
+      'Velit nisi ea do est aute officia occaecat dolor adipisicing reprehenderit.',
+    sourceImage: 'https://randomuser.me/api/portraits/men/14.jpg',
+  },
+  {
+    name: 'Russell',
+    description: 'Nostrud in fugiat laboris irure ex dolore in.',
+    sourceImage: 'https://randomuser.me/api/portraits/men/40.jpg',
+  },
+  {
+    name: 'Clyde',
+    description:
+      'Et reprehenderit magna quis occaecat nisi excepteur aliqua incididunt qui eu non excepteur mollit ad.',
+    sourceImage: 'https://randomuser.me/api/portraits/women/21.jpg',
+  },
+  {
+    name: 'Egbert',
+    description: 'Laboris ea eiusmod do qui sunt et mollit quis esse. ',
+    sourceImage: 'https://randomuser.me/api/portraits/men/46.jpg',
+  },
+  {
+    name: 'Clare',
+    description:
+      'Deserunt sunt proident occaecat eu tempor esse proident labore est aute in laborum.',
+    sourceImage: 'https://randomuser.me/api/portraits/women/81.jpg',
+  },
+  {
+    name: 'Bobbie',
+    description: 'Duis consectetur ad commodo ea aliqua ex fugiat est.',
+    sourceImage: 'https://randomuser.me/api/portraits/men/18.jpg',
+  },
+  {
+    name: 'Angela',
+    description:
+      'Deserunt aute reprehenderit exercitation eiusmod irure excepteur ad ad nisi.',
+    sourceImage: 'https://randomuser.me/api/portraits/women/64.jpg',
+  },
+  {
+    name: 'Elizabeth',
+    description:
+      'Occaecat nulla ut fugiat do aliquip adipisicing officia exercitation esse proident id cupidatat incididunt velit.',
+    sourceImage: 'https://randomuser.me/api/portraits/women/68.jpg',
+  },
+  {
+    name: 'Ted',
+    description: 'Officia nulla pariatur velit commodo in nisi minim magna.',
+    sourceImage: 'https://randomuser.me/api/portraits/men/60.jpg',
+  },
+  {
+    name: 'Caroline',
+    description:
+      'Officia duis fugiat in nulla culpa id laborum proident cupidatat.',
+    sourceImage: 'https://randomuser.me/api/portraits/women/49.jpg',
+  },
+];
---
+++ src/DevUtility/user.js
@@ -0,0 +1,7 @@
+export const DEVuser = {
+  nick: 'Ami',
+  firstName: 'Amelka',
+  lastName: 'Nowakbardzodługienazwisko',
+  email: 'supertajny@gmail.com',
+  phone: '501 234 567',
+};
