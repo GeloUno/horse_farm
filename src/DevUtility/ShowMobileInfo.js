eab630ac5d86460a1cb162411a99c0992c770a9c
GeloUno <geloke59@gmail.com>
2020-10-20 01:43:18 +0200

    Fix error in safari and check other

--- src/DevUtility/ShowMobileInfo.js
+++ src/DevUtility/ShowMobileInfo.js
@@ -3,12 +3,8 @@ import React from 'react';
 export const ShowMobileInfo = () => {
   return (
     <div>
-      <h2>width: {window.screen.width}</h2>
-      <h2>height: {window.screen.height}</h2>
-      <h2>availWidth: {window.screen.availWidth}</h2>
-      <h2>availHeight: {window.screen.availHeight}</h2>
-      <h2>orientation: {window.screen.orientation.type}</h2>
-      <h2>pixelDepth: {window.screen.pixelDepth}</h2>
+      <h2>width: {window.innerWidth}</h2>
+      <h2>height: {window.innerHeight}</h2>
     </div>
   );
 };
