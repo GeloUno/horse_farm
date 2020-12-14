e146c5740a5b7543de845eac71517454af86cbf9
GeloUno <geloke59@gmail.com>
2020-12-14 02:54:03 +0100

    PrivateRoute get data from Redux

--- src/routing/PrivateRoute.js
+++ src/routing/PrivateRoute.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
-import { getCurrentUser, getFirebase } from '../firebase';
 
 const PrivateRoute = ({ component: Component, setLoginModalShow, ...rest }) => {
-  getFirebase();
-  const user = getCurrentUser();
+  // getFirebase();
+  // const user = getCurrentUser();
+  const userAuth = useSelector((state) => state.userAction);
+  const { user } = userAuth;
   return (
     <Route
       {...rest}
       render={(props) =>
-        user ? (
+        user && user.email ? (
           <Component {...props} />
         ) : (
           <Redirect to="/">{setLoginModalShow(true)}</Redirect>
