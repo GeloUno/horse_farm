13e7eb8227d01eb4e637c7ee18491d6af265151c
GeloUno <geloke59@gmail.com>
2020-12-21 10:33:47 +0100

    Add confirm email

---
+++ src/components/Users/ConfirmEmail.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  getCurrentUser,
+  reloadUserAuth,
+  sendVerificationEmail,
+} from '../../firebase';
+import {
+  reloadUserAuthDataAction,
+  userSignOutAction,
+} from '../../redux/actions/userActions';
+
+const handleSignOut = (dispatch) => {
+  dispatch(userSignOutAction);
+};
+
+const ConfirmEmail = ({ email, user }) => {
+  // sendVerificationEmail();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(reloadUserAuthDataAction());
+
+      console.log('This will run after 3 second!', user);
+    }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  const dispatch = useDispatch();
+  return (
+    <div className="confirmEmailContent">
+      <div className="confirmEmailTitle">To już ostatni krok rejestracji</div>
+      <div className="confirmEmailBody">
+        Na podany adres <strong>{email}</strong> wysłaliśmy prośbę o
+        potwierdzenie że podany email podczas rejestracji jest poprawny
+      </div>
+      <div className="confirmEmailButton">
+        <button className="btn btn-green btn-capitalize btn-radius btn-login">
+          Wyślij ponownie
+        </button>
+        <button
+          className="btn btn-red btn-capitalize btn-radius btn-login"
+          onClick={() => {
+            handleSignOut(dispatch);
+          }}
+        >
+          anuluj
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmEmail;
