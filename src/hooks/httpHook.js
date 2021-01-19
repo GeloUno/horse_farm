8423a00f6f8cf22fd63032bfc7507de3e8bb33f0
GeloUno <geloke59@gmail.com>
2021-01-19 10:35:49 +0100

    add http reqest hook

---
+++ src/hooks/httpHook.js
@@ -0,0 +1,42 @@
+//import React from 'react';
+
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+
+const useHttpClient = () => {
+  // const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isErrors, setIsErrors] = useState(false);
+  const [dataResponse, setDataResponse] = useState(null);
+
+  const sendReqestClient = useCallback(
+    async (pathUrl = null, body = null, method = 'get') => {
+      setIsLoading(true);
+      setIsErrors(false);
+      try {
+        let httpClientData = await axios(
+          process.env.REACT_APP_URL_HOST_SERVER + pathUrl,
+          {
+            method: method,
+            data: body,
+          }
+        );
+
+        setDataResponse(httpClientData);
+        console.log('httpClientData :>> ', httpClientData);
+      } catch (error) {
+        setIsErrors(true);
+        if (error.response && error.response.data) {
+          setDataResponse(error.response.data);
+        } else {
+          setDataResponse(error);
+        }
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+  return { isLoading, isErrors, dataResponse, sendReqestClient };
+};
+
+export default useHttpClient;
