65342417992a11f1b467da42e43398077328fad2
GeloUno <geloke59@gmail.com>
2021-04-03 13:16:57 +0200

    Testing and fix

--- src/shared/user.tsx
+++ src/shared/user.tsx
@@ -1,29 +1,31 @@
 import Cookies from 'universal-cookie';
 
+// T
 export const isUserCanBeCreateByPassword = (email: string, isLoading: boolean, isErrors: boolean, dataResponse: unknown, isNewUser: boolean, providerId: string) => {
     return (!!email && providerId === 'password' && isNewUser && !dataResponse && !isLoading && !isErrors)
 }
-
+// T
 export const isUserNeedConfirmEmail = (email: string, isErrors: boolean, emailVerified: boolean) => {
     return (!!email && !isErrors && !emailVerified)
 }
-
+// T
 export const isUserCanSetTokenInCookie = (email: string, emailVerified: boolean) => {
     return (!!email && emailVerified)
 }
-
+// T
 export const isUserCanUpdateDataFromMongoDB = (isLoading: boolean, isErrors: boolean, emailVerified: boolean, dataResponse: unknown) => {
     return (!isLoading && !isErrors && emailVerified && dataResponse)
 }
-
+// T
 export const isUserGetErrorFromDataMongoDB = (isLoading: boolean, isErrors: boolean, dataResponse: unknown) => {
     return (!isLoading && isErrors && dataResponse)
 }
-
+// T
 export const isUserGetCorrectDataAndCanCloseModal = (email: string, isLoading: boolean, isErrors: boolean, dataResponse: unknown, emailVerified: boolean) => {
-    return (email && !isLoading && !isErrors && dataResponse && emailVerified)
+    return (!!email && !isLoading && !isErrors && dataResponse && emailVerified)
 }
 
+// T
 export const isNeedToShowUserBody = (email: string, emailVerified: boolean) => {
     return (!email && !emailVerified)
 }
