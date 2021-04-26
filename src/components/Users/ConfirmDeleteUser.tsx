5d6141bc19d3cfb694716ef3719f3cb377bd4dc7
GeloUno <geloke59@gmail.com>
2021-04-26 16:52:48 +0200

    Split to switch component and detail component

--- src/components/Users/ConfirmDeleteUser.tsx
+++ src/components/Users/ConfirmDeleteUser.tsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { createMuiTheme, ThemeProvider, Container, Box } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { IconButtonConfirm, Message, TypeMessage } from '../Message';
+import { makeStyles } from '@material-ui/core/styles'
 import { useHistory } from 'react-router-dom';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { useDispatch, useSelector } from 'react-redux';
 import { userRemoveAction, userSignOutAction, userRemoveCookieTokenAction } from '../../redux/actions/userActions';
 import { RootState } from '../../redux/store'
-
-// import { useHistory as UseHistory } from 'react-router-dom';
 import { IUserBaseMongoBD } from '../../models/userInterfaces';
+import ConfirmDeleteUserDetail from './ConfirmDeleteUserDetail';
 
 
 
-enum ActionMessage {
+export enum ActionMessage {
     MESSAGE,
     ERROR,
     SUCCESS,
@@ -75,11 +72,6 @@ const ConfirmDeleteUser: React.FC = () => {
     }, [loading])
 
 
-
-    const goToHomePageHandle = () => {
-        history.push('/')
-    }
-
     const logoutUserHandle = () => {
         dispatch(userSignOutAction);
         dispatch(userRemoveCookieTokenAction);
@@ -95,76 +87,22 @@ const ConfirmDeleteUser: React.FC = () => {
         dispatch(userRemoveAction(user))
     }
 
+    const goToHomePageHandle = () => {
+        history.push('/')
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <Container maxWidth={'sm'} className={classes.container}>
 
-                {actionMessage === ActionMessage.MESSAGE && (
-                    <Message title={`Usuwanie konta`}
-                        typeMessage={TypeMessage.ERROR} cancelAction={cancelRemoveUserHandle} confirmAction={confirmRemoveUserHandle}
-                        textButtonConfirm={'Usuń konto'}
-                        iconButtonConfirm={IconButtonConfirm.DELETE_ICON}
-                    >
-                        {<>
-                            <Box p={5}>
-                                Usunięcie konta <strong>{`${user.email}`}</strong> skutkuje trwałym i nie odwracalnym wykasowniem użutkownika oraz wszelkich danych z jego kontem związanych, bez możliwości ich późniejszego odzyskania.
-                            Jeśli nie chesz usunąć konta wciśnij wstecz.
-
-                        </Box>
-                        </>
-                        }</Message>
-                )}
-
-
-                {/* <Message
-                    title='Usuwanie konta'
-                     typeMessage={TypeMessage.INFO} 
-                     textButtonConfirm={'OK'}
-                     iconButtonConfirm={IconButtonConfirm.NO_ICON}                                
-                    >
-                          {<>
-                              <Box p={5} minWidth={'60vh'}>
-                              <CircularProgress color="primary" />
-                              </Box>
-                          </>
-                          }
-                </Message>  */}
-
-                {actionMessage === ActionMessage.LOADING && (
-                    <Box height='60vh' display='flex' alignItems='center' justifyContent='center'>
-                        <CircularProgress color="primary" />
-                    </Box>)}
-
-                {actionMessage === ActionMessage.ERROR && (<Message
-                    title={`Usuwanie konta`}
-                    typeMessage={TypeMessage.ERROR}
-                    textButtonConfirm={'OK'}
-                    iconButtonConfirm={IconButtonConfirm.NO_ICON}
-
-                    confirmAction={() => { goToHomePageHandle() }}>
-                    {<>
-                        <Box p={5} >
-                            Coś poszło nie tak konto <strong>{`${user.email}`}</strong> nie zostało usunięte zapraszam później :-(
-                        </Box>
-                    </>
-                    }</Message>
-                )
-                }
-
-
-                {actionMessage === ActionMessage.SUCCESS && (
-                    <Message
-                        title={`Usuwanie konta`}
-                        typeMessage={TypeMessage.SUCCESS}
-                        textButtonConfirm={'OK'}
-                        iconButtonConfirm={IconButtonConfirm.NO_ICON}
-                        confirmAction={() => { logoutUserHandle() }}>
-                        {<>
-                            <Box p={5} >
-                                Konto <strong>{`${user.email}`}</strong> zostało usunięte :-(
-                        </Box>
-                        </>
-                        }</Message>)}
+                <ConfirmDeleteUserDetail
+                    actionMessage={actionMessage}
+                    confirmAction={confirmRemoveUserHandle}
+                    cancelAction={cancelRemoveUserHandle}
+                    user={user}
+                    goToHomePage={goToHomePageHandle}
+                    logoutUser={logoutUserHandle}
+                />
 
             </Container>
         </ThemeProvider>
---
+++ src/components/Users/ConfirmDeleteUserDetail.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { Box } from '@material-ui/core';
+import { IconButtonConfirm, Message, TypeMessage } from '../Message';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { IUserBaseMongoBD } from '../../models/userInterfaces';
+import { ActionMessage } from './ConfirmDeleteUser';
+
+
+interface ConfirmDeleteUserDetailProps {
+    actionMessage: ActionMessage;
+    cancelAction(): void;
+    confirmAction(): void;
+    user: IUserBaseMongoBD;
+    goToHomePage(): void;
+    logoutUser(): void
+}
+
+
+
+const ConfirmDeleteUserDetail: React.FC<ConfirmDeleteUserDetailProps> = ({
+    actionMessage,
+    cancelAction,
+    confirmAction,
+    user,
+    goToHomePage,
+    logoutUser
+}) => {
+
+    return (
+        <>
+            {actionMessage === ActionMessage.MESSAGE && (
+                <Message title={`Usuwanie konta`}
+                    typeMessage={TypeMessage.ERROR}
+                    cancelAction={cancelAction}
+                    confirmAction={confirmAction}
+                    textButtonConfirm={'Usuń konto'}
+                    iconButtonConfirm={IconButtonConfirm.DELETE_ICON}
+                >
+                    {<>
+                        <Box p={5}>
+                            Usunięcie konta <strong>{`${user.email}`}</strong> skutkuje trwałym i nie odwracalnym wykasowniem użutkownika oraz wszelkich danych z jego kontem związanych, bez możliwości ich późniejszego odzyskania.
+                            Jeśli nie chesz usunąć konta wciśnij wstecz.
+                        </Box>
+                    </>
+                    }</Message>
+            )}
+
+
+            {/* <Message
+                    title='Usuwanie konta'
+                     typeMessage={TypeMessage.INFO} 
+                     textButtonConfirm={'OK'}
+                     iconButtonConfirm={IconButtonConfirm.NO_ICON}                                
+                    >
+                          {<>
+                              <Box p={5} minWidth={'60vh'}>
+                              <CircularProgress color="primary" />
+                              </Box>
+                          </>
+                          }
+                </Message>  */}
+
+            {actionMessage === ActionMessage.LOADING && (
+                <Box height='60vh' display='flex' alignItems='center' justifyContent='center'>
+                    <CircularProgress color="primary" />
+                </Box>)}
+
+            {actionMessage === ActionMessage.ERROR && (<Message
+                title={`Usuwanie konta`}
+                typeMessage={TypeMessage.ERROR}
+                textButtonConfirm={'OK'}
+                iconButtonConfirm={IconButtonConfirm.NO_ICON}
+                confirmAction={goToHomePage}>
+                {<>
+                    <Box p={5} >
+                        Coś poszło nie tak konto <strong>{`${user.email}`}</strong> nie zostało usunięte zapraszam później :-(
+                        </Box>
+                </>
+                }</Message>
+            )
+            }
+
+
+            {actionMessage === ActionMessage.SUCCESS && (
+                <Message
+                    title={`Usuwanie konta`}
+                    typeMessage={TypeMessage.SUCCESS}
+                    textButtonConfirm={'OK'}
+                    iconButtonConfirm={IconButtonConfirm.NO_ICON}
+                    confirmAction={logoutUser}>
+                    {<>
+                        <Box p={5} >
+                            Konto <strong>{`${user.email}`}</strong> zostało usunięte :-(
+                        </Box>
+                    </>
+                    }</Message>)}
+
+        </>
+
+    )
+}
+
+export default ConfirmDeleteUserDetail
+
+
