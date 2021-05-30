7ef33b3278e89892ad10b39c0025f69d0e08f4fd
GeloUno <geloke59@gmail.com>
2021-05-30 12:54:35 +0200

    Mobile header

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { isMobile } from 'react-device-detect'
 import '../App.css';
 import backgroundImageHorse from '../assets/HorseBackground_ttbpbw/HorseBackground_ttbpbw_c_scale,w_1613.png';
 
+import backgroundImageHorseMobile from '../assets/HorseBackground_ttbpbw/HorseBackground_ttbpbw_ar_1_1,c_fill,g_auto__c_scale,w_746.png';
+
+const backgroundImage = () => {
+  console.log(`isMobile`, isMobile)
+  if (isMobile) {
+    return `${backgroundImageHorseMobile}`
+  } else {
+    return `${backgroundImageHorse}`
+  }
+}
 const styleBackgroundImage = {
-  backgroundImage: `url(${backgroundImageHorse})`,
+  backgroundImage: `url(${backgroundImage()})`,
 };
 
 const Header: React.FC = () => {
