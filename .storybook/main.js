81895ba94bddb0b26bd4c8cb82e5bd762c1f74fb
GeloUno <geloke59@gmail.com>
2021-06-11 15:24:06 +0200

    Add view port to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,9 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    '@storybook/addon-viewport',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,19 @@
+import '../src/index.css';
+import '../src/App.css';
+
+import { DEFAULT_VIEWPORT, INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 export const parameters = {
+  // import: './App.css',
+  viewport: {
+    viewports: MINIMAL_VIEWPORTS,
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+
   },
 }
