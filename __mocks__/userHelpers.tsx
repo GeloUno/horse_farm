01b19ee01a2bcc1596ac30d3b96727ce790e2e56
GeloUno <geloke59@gmail.com>
2021-06-24 12:28:30 +0200

    Add __Mock__ folder

---
+++ __mocks__/userHelpers.tsx
@@ -0,0 +1,14 @@
+export const handleSignOut = () => {
+    console.log(`MOCK FUN -> intervalGetEmailConfirmStatus`)
+};
+export const handleSendVerificationEmail = () => {
+    console.log(`MOCK FUN -> intervalGetEmailConfirmStatus`)
+};
+const reloadConfirmEmail = () => {
+    console.log(`MOCK FUN -> intervalGetEmailConfirmStatus`)
+};
+const intervalGetEmailConfirmStatus = () => {
+    console.log(`MOCK FUN -> intervalGetEmailConfirmStatus`)
+}
+
+export default intervalGetEmailConfirmStatus;
